d6f872c13134a652db772204aae009eab01d21f6
Vandewynckel Kristof <98815092+KristofVandewynckel@users.noreply.github.com>
2022-05-04 16:23:59 +0200

    fixing

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask, render_template, request
+from sympy import N
+import pickle
+import numpy as np
 
 app = Flask(__name__)
+
+model = pickle.load(open('model/model.pickle', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route("/predict")
+@app.route('/predict', methods=['GET','POST'])
 def predict():
-    Area = request.form.get("Area", type=int)
-    Type = request.form.get("Property Type", type=int)
-    Rooms = request.form.get("Number of rooms", type=int)
-    Zip = request.form.get("Zip Code", type=int)
-
-    entry = "Hello"
     return render_template('form.html')
 
-@app.route("/result")
+@app.route('/result')
 def result():
-    entry = "Simsalabim"
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    entry = output
     return render_template('result.html', entry=entry)
 
 if __name__ == '__main__':
