087f784ec26e336c9099dec807a77381d5f2eb8b
Vandewynckel Kristof <98815092+KristofVandewynckel@users.noreply.github.com>
2022-05-05 14:36:34 +0200

    linking it all together

--- app.py
+++ app.py
@@ -15,9 +15,61 @@ def home():
 def predict():
     return render_template('form.html')
 
-@app.route('/result')
+@app.route('/result', methods=['GET','POST'])
 def result():
-    int_features = [int(x) for x in request.form.values()]
+    bedrooms = request.form.get("Rooms", type=int)
+    liv_area = request.form.get("Living Area", type=int)
+    surface_area = request.form.get("Surface Land Area", type=int)
+    subtype = request.form.get("Type")
+    house = 0
+    villa = 0
+    farmhouse = 0
+    mansion = 0
+    mixed_use = 0
+    if subtype == "House":
+        house = 1
+    elif subtype == "Villa":
+        villa = 1
+    elif subtype == "Farmhouse":
+        farmhouse = 1
+    elif subtype == "Mansion":
+        mansion = 1
+    elif subtype == "Mixed_use":
+        mixed_use = 1
+
+    kitchen = request.form.get("Kitchen Equipped?")
+    if kitchen == "Yes":
+        kitchen = 1
+    else:
+        kitchen = 0
+    fireplace = request.form.get("Open fireplace")
+    if fireplace == "Yes":
+        fireplace = 1
+    else:
+        fireplace = 0  
+    terrace = request.form.get("Terrace")
+    if terrace == "Yes":
+        terrace = 1
+    else:
+        terrace = 0  
+    garden = request.form.get("Garden")
+    if garden == "Yes":
+        garden = 1
+    else:
+        garden = 0   
+    pool = request.form.get("Pool")
+    if pool== "Yes":
+        pool = 1
+    else:
+        pool = 0  
+    condition = request.form.get("Condition")
+    if condition == "To be renovated":
+        condition = 1
+    else:
+        condition = 0
+    
+    request_values = [bedrooms,liv_area,surface_area,farmhouse, house, mansion, mixed_use, villa, kitchen,fireplace,terrace,garden,pool,condition]
+    int_features = [int(x) for x in request_values]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     output = round(prediction[0], 2)
