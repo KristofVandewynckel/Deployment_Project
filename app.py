38e02370321a6bbe890e53ff8cc4ce5b4fac33de
Vandewynckel Kristof <98815092+KristofVandewynckel@users.noreply.github.com>
2022-05-06 12:01:19 +0200

    finishing

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# We import the necessary modules.
+
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -5,22 +7,31 @@ import math
 
 app = Flask(__name__)
 
+# We open the model saved in the 'model' folder
 model = pickle.load(open('model/model.pickle', 'rb'))
 
+# We define our homepage
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# We define our predict page
 @app.route('/predict', methods=['GET','POST'])
 def form():
     return render_template('form.html')
 
+# We define our result page and predict function.
 @app.route('/result', methods=['GET','POST'])
 def predict():
+
+    # We create variables from the input given in our form.
     bedrooms = request.form.get("Rooms", type=int)
     liv_area = request.form.get("Living Area", type=int)
     surface_area = request.form.get("Surface Land Area", type=int)
     subtype = request.form.get("Type")
+
+        # Since we have multiple subtypes that have changed to dummies, we manually 
+        # add 0 and 1 depending on the input.
     house = 0
     villa = 0
     farmhouse = 0
@@ -68,10 +79,15 @@ def predict():
     else:
         condition = 0
     
+    # We sort all the variables in an list, loop over them and put them into an array
     request_values = [bedrooms,liv_area,surface_area,farmhouse, house, mansion, mixed_use, villa, kitchen,fireplace,terrace,garden,pool,condition]
     int_features = [int(x) for x in request_values]
     final_features = [np.array(int_features)]
+
+    # We then apply the model to our features.
     prediction = model.predict(final_features)
+
+    # Round the output to 1000s and return the result to our result page.
     output = int(math.ceil(prediction / 1000)) *1000
     entry = output
     return render_template('result.html', entry=entry)
